7b77fa4af1f312e565dd1069010f9553f91f5beb
Christian Leopold <christian.leopold@student.tugraz.at>
2016-06-01 09:49:06 +0200

    First Testcase CalculatorTest.java

---
+++ app/src/androidTest/java/catrobat/calculator/uitest/CalculatorTest.java
@@ -0,0 +1,25 @@
+package catrobat.calculator.uitest;
+
+/**
+ * Created by chrl on 01/06/16.
+ *
+ * Basic Testcase
+ */
+public class CalculatorTest extends CalculatorUITestTemplate {
+
+    public void testButtons() throws Exception {
+
+        for(int i = 0; i< 10; i++)
+        {
+            solo.clickOnText( Integer.toString(i));
+        }
+
+        solo.clickOnText("+");
+        solo.clickOnText("-");
+        solo.clickOnText("*");
+        solo.clickOnText("/");
+        solo.clickOnText("C");
+        solo.clickOnText("=");
+
+    }
+}
