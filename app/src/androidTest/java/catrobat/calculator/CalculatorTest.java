94895102015524db401ade6212cacb0cf814c81c
Christian Leopold <christian.leopold@student.tugraz.at>
2016-06-01 09:27:49 +0200

    initia commit

---
+++ app/src/androidTest/java/catrobat/calculator/CalculatorTest.java
@@ -0,0 +1,26 @@
+package catrobat.calculator;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * Created by chrl on 01/06/16.
+ */
+public class CalculatorTest extends ActivityInstrumentationTestCase2 {
+
+    public CalculatorTest() {
+        super(Calculator.class);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
