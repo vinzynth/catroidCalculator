7da0a703fa5b5468adbb59587da88ec7666148a1
Christian Leopold <christian.leopold@student.tugraz.at>
2016-06-01 10:46:01 +0200

    Added OnClickListener

--- app/src/androidTest/java/catrobat/calculator/uitest/CalculatorTest.java
+++ app/src/androidTest/java/catrobat/calculator/uitest/CalculatorTest.java
@@ -2,16 +2,15 @@ package catrobat.calculator.uitest;
 
 /**
  * Created by chrl on 01/06/16.
- *
+ * <p/>
  * Basic Testcase
  */
 public class CalculatorTest extends CalculatorUITestTemplate {
 
     public void testButtons() throws Exception {
 
-        for(int i = 0; i< 10; i++)
-        {
-            solo.clickOnText( Integer.toString(i));
+        for (int i = 0; i < 10; i++) {
+            solo.clickOnText(Integer.toString(i));
         }
 
         solo.clickOnText("+");
--- app/src/main/java/catrobat/calculator/Calculator.java
+++ app/src/main/java/catrobat/calculator/Calculator.java
@@ -1,13 +1,53 @@
 package catrobat.calculator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class Calculator extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Calculator extends Activity implements OnClickListener {
+
+    private Button buttonPlus, buttonMinus, buttonDivide, buttonMultiply, buttonClear, buttonEquals;
+    private Collection<Button> numberButtons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        buttonPlus = (Button) findViewById(R.id.button_add);
+        buttonMinus = (Button) findViewById(R.id.button_sub);
+        buttonDivide = (Button) findViewById(R.id.button_div);
+        buttonMultiply = (Button) findViewById(R.id.button_mul);
+        buttonClear = (Button) findViewById(R.id.button_c);
+        buttonEquals = (Button) findViewById(R.id.button_eq);
+
+        buttonPlus.setOnClickListener(this);
+        buttonMinus.setOnClickListener(this);
+        buttonDivide.setOnClickListener(this);
+        buttonMultiply.setOnClickListener(this);
+        buttonClear.setOnClickListener(this);
+        buttonEquals.setOnClickListener(this);
+
+        numberButtons = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            String buttonName = "button_" + i;
+            int id = getResources().getIdentifier(buttonName, "id", R.class.getPackage().getName());
+            Button button = (Button) findViewById(id);
+            button.setOnClickListener(this);
+            numberButtons.add(button);
+        }
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
